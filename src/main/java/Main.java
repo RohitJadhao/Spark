89b71708bfc9fb0f8c903a8ddc9327a3ca5c22c4
Rohit V Jadhao <rohitjadhao92@gmail.com>
2020-07-17 21:37:49 +0530

    Removed comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
-/*import org.apache.spark.SparkConf;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
-import java.util.Arrays;*/
+import java.util.Arrays;
 import org.apache.avro.Schema;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
